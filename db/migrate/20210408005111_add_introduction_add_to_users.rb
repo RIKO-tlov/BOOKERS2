9ca61d0c4ff65c88db3e18f14dd2b6ce430614ad
RIKO-tlov <bluebyrair@outlook.jp>
2021-04-15 06:44:34 +0000

    [delete migration files]

--- db/migrate/20210408005111_add_introduction_add_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddIntroductionAddToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :introduction_id, :text
-  end
-end
--- db/migrate/20210411042142_remove_introduction_id_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveIntroductionIdFromUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :introduction_id, :text
-  end
-end
