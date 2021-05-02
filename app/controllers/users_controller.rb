4bcae76e068b8f177d7b3abd5cd593be7f569d82
RIKO-tlov <bluebyrair@outlook.jp>
2021-05-02 12:17:48 +0000

    [finish]assignment3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,9 @@ class UsersController < ApplicationController
   end
 
   def show
+     @book = Book.new
      @user = User.find(params[:id])
      @books = @user.books
-     @book = Book.new
   end
 
   def edit
