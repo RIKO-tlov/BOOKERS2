0c244fb243fc70a16cde5696e3b839ddcb2b1dbe
RIKO-tlov <bluebyrair@outlook.jp>
2021-05-09 08:15:04 +0000

    [finish]assignment8

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     #スーパークラス(devise)のcreateアクションを呼ぶ
     super
-    #WelcomeMailerクラスのsend_when_signupメソッドを呼び、POSTから受け取ったuserのemailとnameを渡す
-    WelcomeMailer.send_when_signup(params[:user][:email],params[:user][:name]).deliver
+      if @user.save
+      #ThanksMailerクラスのsend_when_signupメソッドを呼び、POSTから受け取ったuserのemailとnameを渡す
+        ThanksMailer.send_when_signup(params[:user][:email],params[:user][:name]).deliver
+      end
   end
 end
--- app/mailers/thanks_mailer.rb
+++ app/mailers/thanks_mailer.rb
@@ -1,4 +1,4 @@
-class WelcomeMailer < ApplicationMailer
+class ThanksMailer < ApplicationMailer
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
